c524bf8f7a593ff5116c1e60e5a1f957994baaf2
unknown <02501jm@gmail.com>
2019-10-20 21:40:13 -0600

    Adds post request for items

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const Item = require('./models/item');
 
 const app = express();
 
-mongoose.connect("mongodb+srv://john:SinfoniaAcentuar@cluster0-hfzas.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true})
+mongoose.connect("mongodb+srv://john:SinfoniaAcentuar@cluster0-hfzas.mongodb.net/commerce?retryWrites=true&w=majority", { useNewUrlParser: true})
   .then(() => {
     console.log('Connected to database');
   })
   .catch(() => {
-    console.log('Connection Failed!')
-  })
-;
+    console.log('Connection Failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -23,15 +23,22 @@ app.use((req, res, next) => {
     next();
 });
 
+app.post("/api/items", (req, res) => {
+   console.log(req.body);
+  // it = JSON.parse(req.body);
+  // const item = new Item({
+  //   name: it.name,
+  //   description: it.description
+  // });
+  // item.save();
+  res.status(201).json({
+    message: 'Item added succesfully'
+  });
+});
+
+app.get("/api/items", (req, res) => {
+  console.log('Entered getItems')
+});
+
 
 module.exports = app;
-
-
-
-
-
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port)
---
+++ models/item.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const itemSchema = mongoose.Schema({
+    name: {type: String, required: true, default: 'item_name'},
+    description: {type: String, required: true}
+});
+
+module.exports = mongoose.model('item', itemSchema)
